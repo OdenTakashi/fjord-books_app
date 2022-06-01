3b867990a43d5ba4e2544b104b0317cf581336aa
OdenTakashi <odentakashi@icloud.com>
2022-06-01 16:08:13 +0900

    letter_opener_webのセットアップ

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :books
 
   root to: 'users#index'
-
   get 'users/:id', to: 'users#show'
+
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
