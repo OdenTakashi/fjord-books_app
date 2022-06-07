fc8df07b25381129bc0c7da2f4cc36b36c1e4026
OdenTakashi <odentakashi@icloud.com>
2022-06-07 20:49:13 +0900

    friendshipコントローラーにdestroy,createメソッドを追加

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -6,6 +6,8 @@ class FriendshipsController < ApplicationController
     end
 
     def destroy
-
+        user = Friendship.find(params[:id]).followed
+        current_user.unfollow(user)
+        redirect_to user
     end
 end
