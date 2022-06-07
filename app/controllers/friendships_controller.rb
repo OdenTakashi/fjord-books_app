7f733949883044b9129087bf1ca02d9bbf937cfd
OdenTakashi <odentakashi@icloud.com>
2022-06-07 16:11:09 +0900

    friendモデルのcreateメソッドを追加
    createメソッドのみ追加、destroyメソッドは未実装

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,2 +1,11 @@
 class FriendshipsController < ApplicationController
+    def create
+        user = User.find(params[:followed_id])
+        current_user.follow(user)
+        redirect_to user
+    end
+
+    def destroy
+
+    end
 end
