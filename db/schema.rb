315833c9b3868249abd806010d1ea5c42c9be7d7
OdenTakashi <odentakashi@icloud.com>
2022-06-14 14:32:13 +0900

    migrationファイルの適用
    AddColumnsAboutUserDetailsToUsers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_13_091233) do
+ActiveRecord::Schema.define(version: 2022_06_14_052800) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
