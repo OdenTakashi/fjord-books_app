bbfa28cef3170862023bacc4375cac9a7cdce6c0
OdenTakashi <odentakashi@icloud.com>
2022-06-16 18:44:36 +0900

    フォロー関連の翻訳を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,14 +10,14 @@ class UsersController < ApplicationController
   end
 
   def followings
-    @title = 'Followings'
+    @title = "#{t(:followings)}"
     @user  = User.find(params[:id])
     @users = @user.followings
     render 'show_follow'
   end
 
   def followers
-    @title = 'Followers'
+    @title = "#{t(:followers)}"
     @user  = User.find(params[:id])
     @users = @user.followers
     render 'show_follow'
