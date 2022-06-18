ef90a29fa36176a3965852aca52ac4de3d72a016
OdenTakashi <odentakashi@icloud.com>
2022-06-18 12:18:59 +0900

    reportのmigrationファイルを一つにまとめた

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_17_112824) do
+ActiveRecord::Schema.define(version: 2022_06_18_031502) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -59,11 +59,11 @@ ActiveRecord::Schema.define(version: 2022_06_17_112824) do
   end
 
   create_table "reports", force: :cascade do |t|
-    t.string "title"
-    t.text "context"
+    t.string "title", null: false
+    t.text "context", null: false
+    t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
     t.index ["user_id"], name: "index_reports_on_user_id"
   end
 
