d135c9a19a80b202b01c0437dd616b6bcff1ef39
OdenTakashi <odentakashi@icloud.com>
2022-06-22 14:39:30 +0900

    controllerを作成

---
+++ app/controllers/books/comments_controller.rb
@@ -0,0 +1,20 @@
+class Books::CommentsController < ApplicationController
+    def create
+        @book = Book.find(params[:book_id])
+        @comment = @book.comments.build(comment_params)
+        @comment.user_id = current_user.id
+        @comment.save
+        redirect_to book_url(@book)
+    end
+
+    def destroy
+        @comment = Comment.find(params[:id])
+        @comment.destroy
+        redirect_to book_url(Book.find(params[:book_id]))
+    end
+
+    private
+    def comment_params
+        params.require(:comment).permit(:body, :created_at)
+    end
+end
---
+++ app/controllers/reports/comments_controller.rb
@@ -0,0 +1,20 @@
+class Reports::CommentsController < ApplicationController
+    def create
+        @report = Report.find(params[:report_id])
+        @comment = @report.comments.build(comment_params)
+        @comment.user_id = current_user.id
+        @comment.save
+        redirect_to report_url(@report)
+    end
+
+    def destroy
+        @comment = Comment.find(params[:id])
+        @comment.destroy
+        redirect_to report_url(Report.find(params[:report_id]))
+    end
+
+    private
+    def comment_params
+        params.require(:comment).permit(:body, :created_at)
+    end
+end
