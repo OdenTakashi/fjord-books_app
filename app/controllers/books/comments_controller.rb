f9d63ff59490c58838fc4a286381780468852c1b
OdenTakashi <odentakashi@icloud.com>
2022-06-22 14:43:02 +0900

    rubocopを通した

--- app/controllers/books/comments_controller.rb
+++ app/controllers/books/comments_controller.rb
@@ -1,20 +1,23 @@
+# frozen_string_literal: true
+
 class Books::CommentsController < ApplicationController
-    def create
-        @book = Book.find(params[:book_id])
-        @comment = @book.comments.build(comment_params)
-        @comment.user_id = current_user.id
-        @comment.save
-        redirect_to book_url(@book)
-    end
+  def create
+    @book = Book.find(params[:book_id])
+    @comment = @book.comments.build(comment_params)
+    @comment.user_id = current_user.id
+    @comment.save
+    redirect_to book_url(@book)
+  end
 
-    def destroy
-        @comment = Comment.find(params[:id])
-        @comment.destroy
-        redirect_to book_url(Book.find(params[:book_id]))
-    end
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to book_url(Book.find(params[:book_id]))
+  end
 
-    private
-    def comment_params
-        params.require(:comment).permit(:body, :created_at)
-    end
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body, :created_at)
+  end
 end
--- app/controllers/reports/comments_controller.rb
+++ app/controllers/reports/comments_controller.rb
@@ -1,20 +1,23 @@
+# frozen_string_literal: true
+
 class Reports::CommentsController < ApplicationController
-    def create
-        @report = Report.find(params[:report_id])
-        @comment = @report.comments.build(comment_params)
-        @comment.user_id = current_user.id
-        @comment.save
-        redirect_to report_url(@report)
-    end
+  def create
+    @report = Report.find(params[:report_id])
+    @comment = @report.comments.build(comment_params)
+    @comment.user_id = current_user.id
+    @comment.save
+    redirect_to report_url(@report)
+  end
 
-    def destroy
-        @comment = Comment.find(params[:id])
-        @comment.destroy
-        redirect_to report_url(Report.find(params[:report_id]))
-    end
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to report_url(Report.find(params[:report_id]))
+  end
 
-    private
-    def comment_params
-        params.require(:comment).permit(:body, :created_at)
-    end
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body, :created_at)
+  end
 end
