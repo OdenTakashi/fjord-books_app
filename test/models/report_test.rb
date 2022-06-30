356e67e606541338f8f391064da79b8cc2e36331
OdenTakashi <odentakashi@icloud.com>
2022-06-30 16:51:16 +0900

    userモデルとreportyモデルのテストファイルを追加

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,19 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "created_on" do 
+  user = User.create!(name: "sample", email: "sample@example.com", password: "password")
+    travel_to Time.zone.local(2022, 6,29) do 
+      report = user.reports.create!(title: "test", content: "sample")
+      assert_equal Date.today, report.created_on
+    end
+  end
+
+  test "editable?" do
+    me = User.create!(name: "sample", email: "samp@example.com", password: "password")
+    you = User.create!(name: "sample", email: "sampl@example.com", password: "password")
+    report = me.reports.create!(title: "test", content: "sample")
+    assert report.editable?(me)
+    assert_not report.editable?(you)
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,40 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "name_or_email" do
+    user = users(:test)
+    assert_equal "test@example.com", user.name_or_email
+  end
+
+  test "follow" do
+    me = users(:me)
+    she = users(:she)
+    assert_not me.following?(she)
+    me.follow(she)
+    assert me.following?(she)
+  end
+
+  test "unfollow" do
+    me = users(:me)
+    she = users(:she)
+    me.follow(she)
+    assert me.following?(she)
+    me.unfollow(she)
+    assert_not me.following?(she)
+  end
+
+  test "following?" do
+    me = users(:me)
+    she = users(:she)
+    me.follow(she)
+    assert me.following?(she)
+  end
+
+  test "followed_by?" do
+    me = users(:me)
+    she = users(:she)
+    me.follow(she)
+    assert she.followed_by?(me)
+  end
+
 end
