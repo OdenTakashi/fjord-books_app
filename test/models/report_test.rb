0da804b1292fb810f0884c29fabbe421c938ca8d
OdenTakashi <odentakashi@icloud.com>
2022-07-01 21:03:44 +0900

    modelのインスタンスメソッドのテストを修正

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -4,17 +4,17 @@ require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
   test "created_on" do 
-  user = User.create!(name: "sample", email: "sample@example.com", password: "password")
+  user = User.create!(name: "Mike", email: "mike@example.com", password: "password")
     travel_to Time.zone.local(2022, 6,29) do 
-      report = user.reports.create!(title: "test", content: "sample")
+      report = user.reports.create!(title: "lunch", content: "yakiniku")
       assert_equal Date.today, report.created_on
     end
   end
 
   test "editable?" do
-    me = User.create!(name: "sample", email: "samp@example.com", password: "password")
-    you = User.create!(name: "sample", email: "sampl@example.com", password: "password")
-    report = me.reports.create!(title: "test", content: "sample")
+    me = User.create!(name: "Dave", email: "dave@example.com", password: "password")
+    you = User.create!(name: "Bieber", email: "bieber@example.com", password: "password")
+    report = me.reports.create!(title: "dinner", content: "carry")
     assert report.editable?(me)
     assert_not report.editable?(you)
   end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,39 +4,41 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   test "name_or_email" do
-    user = users(:test)
-    assert_equal "test@example.com", user.name_or_email
+    user_test1 = users(:wakisaka)
+    user_test2 = users(:naoki)
+    assert_equal "wakisaka@example.com", user_test1.name_or_email
+    assert_equal "Naoki", user_test2.name_or_email
   end
 
   test "follow" do
-    me = users(:me)
-    she = users(:she)
-    assert_not me.following?(she)
-    me.follow(she)
-    assert me.following?(she)
+    me = users(:wakisaka)
+    he = users(:naoki)
+    assert_not me.following?(he)
+    me.follow(he)
+    assert me.following?(he)
   end
 
   test "unfollow" do
-    me = users(:me)
-    she = users(:she)
-    me.follow(she)
-    assert me.following?(she)
-    me.unfollow(she)
-    assert_not me.following?(she)
+    me = users(:wakisaka)
+    he = users(:naoki)
+    me.follow(he)
+    assert me.following?(he)
+    me.unfollow(he)
+    assert_not me.following?(he)
   end
 
   test "following?" do
-    me = users(:me)
-    she = users(:she)
-    me.follow(she)
-    assert me.following?(she)
+    me = users(:wakisaka)
+    he = users(:naoki)
+    me.follow(he)
+    assert me.following?(he)
   end
 
   test "followed_by?" do
-    me = users(:me)
-    she = users(:she)
-    me.follow(she)
-    assert she.followed_by?(me)
+    me = users(:wakisaka)
+    he = users(:naoki)
+    me.follow(he)
+    assert he.followed_by?(me)
   end
 
 end
