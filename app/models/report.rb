a920803121e8b2e2cad644f5a55299ee0a427842
OdenTakashi <odentakashi@icloud.com>
2022-07-06 14:06:03 +0900

    view側の権限チェックを戻した

--- app/models/report.rb
+++ app/models/report.rb
@@ -3,8 +3,4 @@
 class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
-
-  def edit_permission(login_user)
-    user_id == login_user.id
-  end
 end
